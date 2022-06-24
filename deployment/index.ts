73cf9d14a8853dee79cf6ba84be4457e2860cc05
Cedric Kring <cedric.kring@gmail.com>
2022-06-24 12:02:53 +0200

    add google compute instance deployment

--- deployment/index.ts
+++ deployment/index.ts
@@ -1,10 +1,26 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as gcp from "@pulumi/gcp";
+import * as pulumi from '@pulumi/pulumi';
+import * as gcp from '@pulumi/gcp';
 
-// Create a GCP resource (Storage Bucket)
-const bucket = new gcp.storage.Bucket("my-bucket", {
-    location: 'US'
+const network = new gcp.compute.Network('network');
+const computeFirewall = new gcp.compute.Firewall('firewall', {
+    network: network.id,
+    sourceRanges: ['0.0.0.0/0'],
+    allows: [{
+        protocol: 'tcp',
+        ports: ['22', '5000'],
+    }],
 });
 
-// Export the DNS name of the bucket
-export const bucketName = bucket.url;
+// Create a Virtual Machine Instance
+const computeInstance = new gcp.compute.Instance('instance', {
+    machineType: 'e2-medium',
+    bootDisk: {initializeParams: {image: 'debian-cloud/debian-11'}},
+    networkInterfaces: [{
+        network: network.id,
+        // accessConfigus must include a single empty config to request an ephemeral IP
+        accessConfigs: [{}],
+    }],
+});
+
+export const instanceName = computeInstance.name;
+export const instanceIP = computeInstance.networkInterfaces.apply(ni => ni[0]?.accessConfigs?.[0].natIp);
