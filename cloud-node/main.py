87692eabf7e83ef4547848005052763fad113c85
Cedric Kring <cedric.kring@gmail.com>
2022-06-29 08:16:12 +0200

    add basic communication from fog to cloud node with zeromq

--- cloud-node/main.py
+++ cloud-node/main.py
@@ -1,5 +1,26 @@
+import json
+
+import zmq
+
+
 def main():
-    print('Cloud Node')
+    context = zmq.Context(1)
+
+    socket = context.socket(zmq.ROUTER)  # ROUTER
+    socket.bind("tcp://*:5555")
+
+    while True:
+        msg = socket.recv_multipart()
+        print(msg)
+        if not msg:
+            break
+
+        data = json.loads(msg[2])
+
+        response = msg[:2] + [b'warm' if data['temperature'] >= 20 else b'cold',
+                              b'humid' if data['humidity'] >= .5 else b'arid']
+
+        socket.send_multipart(response)
 
 
 if __name__ == '__main__':
--- fog-node/main.py
+++ fog-node/main.py
@@ -1,6 +1,38 @@
-def main():
-    print('Fog Node')
+from random import randint
+import time
+import zmq
+from sensor import TemperatureSensor, HumiditySensor
 
+context = zmq.Context(1)
+worker = context.socket(zmq.REQ)
+
+identity = "%04X-%04X" % (randint(0, 0x10000), randint(0, 0x10000))
+worker.setsockopt_string(zmq.IDENTITY, identity)
+worker.connect("tcp://localhost:5555")
+
+temperature_sensor = TemperatureSensor()
+humidity_sensor = HumiditySensor()
+
+
+# TODO handshake
+
+def main():
+    while True:
+        temperature, humidity = temperature_sensor.sample(), humidity_sensor.sample()
+        worker.send_json({
+            'temperature': temperature,
+            'humidity': humidity
+        })
+
+        msg = worker.recv_multipart()
+        if not msg:
+            break
+
+        print(msg)
+
+
+y
+time.sleep(10)  # Do some heavy work
 
 if __name__ == '__main__':
     main()
