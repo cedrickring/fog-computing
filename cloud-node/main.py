52c26c39769ff861ee146bc5ce33589aac989f68
kronsbein <m.kronsbein@campus.tu-berlin.de>
2022-07-01 18:23:51 +0200

    Added weather prediction in cloud node main.py

--- cloud-node/main.py
+++ cloud-node/main.py
@@ -1,13 +1,16 @@
 import json
-
+import pickle
 import zmq
-
+from sklearn.preprocessing import MinMaxScaler
 
 def main():
     context = zmq.Context(1)
 
     socket = context.socket(zmq.ROUTER)  # ROUTER
     socket.bind("tcp://*:5555")
+
+    filepath = "../models/random_forest.sav"
+    classifier, scaler, label_encoder = pickle.load(open(filepath, 'rb'))
 
     i = 0
     while True:
@@ -18,10 +21,10 @@ def main():
 
         data = json.loads(msg[2])
         print(data)
-
-        prediction = f'Sunny{i}'
+        norm_data = scaler.transform([list(data.values())])
+        prediction = label_encoder.inverse_transform(classifier.predict(norm_data))
         i += 1
-        response = msg[:2] + [prediction.encode()]
+        response = msg[:2] + [prediction[0].encode()]
 
         socket.send_multipart(response)
 
